e9c264be4ab8860002f68529b5df19e207dad96c
Ashish <ashish.adhikari@bison.howad.edu>
2021-04-29 16:40:19 -0400

    start writing with simulation parameters

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+
+'''SIMULATION PARAMETERS'''
+
+#Content priority profile (0-5)
+ALPHA = 0
+
+#number of UEs in the network (100-300)
+NUM_UE = 0
+
+#number of popular contents (20)
+NUM_POP_CONTENT =  0
+
+#number of D2D clusters (4)
+NUM_D2D_CLUSTERS = 0
+
+# lambda_x the user content access request rate (0.05)
+UE_REQUEST_RATE = 0
+
+#mean contact rate between two users (0.05 - 0.25)
+UE_CONTACT_RATE = 0
+
+
+
